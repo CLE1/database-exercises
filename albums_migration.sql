7944c67aecae1efe4362bb9969fc4e7aef4d6540
Christopher <espinoza.christopher117@gmail.com>
2021-07-27 09:33:53 -0500

    break

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,18 @@
-USE codeup_test_db;
+USE
+codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
+
+CREATE
+    TABLE albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(50)  NOT NULL,
+    name         VARCHAR(100) NOT NULL,
+    release_date DATE,
+    sales        FLOAT,
+    genre        VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DESCRIBE albums;
